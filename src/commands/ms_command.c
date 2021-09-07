07e3cba8f75cbe5f22ad78b3e44bf49beb556d30
mshmelly <mshmelly@student.21-school.ru>
2021-09-07 18:28:25 +0300

    commit on my mac

---
+++ src/commands/ms_command.c
@@ -0,0 +1,25 @@
+#include "../../inc/minishell.h"
+
+int	ms_chech_builtins(char *key)
+{
+	if (!ft_strcmp(key, "echo"))
+		return (0);
+	else if (!ft_strcmp(key, "cd"))
+		return (0);
+	else if (!ft_strcmp(key, "pwd"))
+		return (0);
+	else if (!ft_strcmp(key, "export"))
+		return (0);
+	else if (!ft_strcmp(key, "unset"))
+		return (0);
+	else if (!ft_strcmp(key, "exit"))
+		return (0);
+	else 
+		return(1);
+}
+
+//int main(int agrc, char **agrv)
+//{
+//	printf("%d\n", ms_chech_builtins(agrv[1]));
+//	return(0);
+//}

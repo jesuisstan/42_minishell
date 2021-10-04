90448214fff6f06725def679af8806f41eeed311
Melinda Shmelly <mshmelly@va-e5.kzn.21-school.ru>
2021-10-04 21:26:20 +0300

    merge parsing + master

---
+++ cmake-build-debug/CMakeFiles/minishell.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/mshmelly/21/minishell/src/builtins/ms_cd.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_cd.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_cd_utils.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_cd_utils.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_echo.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_echo.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_env.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_env.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_exit.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_exit.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_export.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_export.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_pwd.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_pwd.c.o"
+  "/Users/mshmelly/21/minishell/src/builtins/ms_unset.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/builtins/ms_unset.c.o"
+  "/Users/mshmelly/21/minishell/src/commands/ms_builtin.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/commands/ms_builtin.c.o"
+  "/Users/mshmelly/21/minishell/src/commands/ms_command.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/commands/ms_command.c.o"
+  "/Users/mshmelly/21/minishell/src/commands/ms_path.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/commands/ms_path.c.o"
+  "/Users/mshmelly/21/minishell/src/commands/ms_pipe.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/commands/ms_pipe.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_check_redirect.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_check_redirect.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_clone_envp.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_clone_envp.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_cut_arguments.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_cut_arguments.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_manage_dollar.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_manage_dollar.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_parse.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_parse.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_pass_whitespaces.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_pass_whitespaces.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_protoparse.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_protoparse.c.o"
+  "/Users/mshmelly/21/minishell/src/parser/ms_purify_argument.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/parser/ms_purify_argument.c.o"
+  "/Users/mshmelly/21/minishell/src/utils/ms_arglist_to_array.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/utils/ms_arglist_to_array.c.o"
+  "/Users/mshmelly/21/minishell/src/utils/ms_arrlen.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/utils/ms_arrlen.c.o"
+  "/Users/mshmelly/21/minishell/src/utils/ms_envplist_to_array.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/utils/ms_envplist_to_array.c.o"
+  "/Users/mshmelly/21/minishell/src/utils/ms_find_envp.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/utils/ms_find_envp.c.o"
+  "/Users/mshmelly/21/minishell/src/utils/ms_malloc_x.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/utils/ms_malloc_x.c.o"
+  "/Users/mshmelly/21/minishell/src/utils/ms_return_error.c" "/Users/mshmelly/21/minishell/cmake-build-debug/CMakeFiles/minishell.dir/src/utils/ms_return_error.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/readline"
+  "../src/libft"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/minishell.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minishell.dir/src/builtins/ms_cd.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_cd_utils.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_echo.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_env.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_exit.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_export.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_pwd.c.o"
+  "CMakeFiles/minishell.dir/src/builtins/ms_unset.c.o"
+  "CMakeFiles/minishell.dir/src/commands/ms_builtin.c.o"
+  "CMakeFiles/minishell.dir/src/commands/ms_command.c.o"
+  "CMakeFiles/minishell.dir/src/commands/ms_path.c.o"
+  "CMakeFiles/minishell.dir/src/commands/ms_pipe.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_check_redirect.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_clone_envp.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_cut_arguments.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_manage_dollar.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_parse.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_pass_whitespaces.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_protoparse.c.o"
+  "CMakeFiles/minishell.dir/src/parser/ms_purify_argument.c.o"
+  "CMakeFiles/minishell.dir/src/utils/ms_arglist_to_array.c.o"
+  "CMakeFiles/minishell.dir/src/utils/ms_arrlen.c.o"
+  "CMakeFiles/minishell.dir/src/utils/ms_envplist_to_array.c.o"
+  "CMakeFiles/minishell.dir/src/utils/ms_find_envp.c.o"
+  "CMakeFiles/minishell.dir/src/utils/ms_malloc_x.c.o"
+  "CMakeFiles/minishell.dir/src/utils/ms_return_error.c.o"
+  "minishell"
+  "minishell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minishell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

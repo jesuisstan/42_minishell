92b899e06f20d5aac0019a82a9b53181d57737bd
Melinda Shmelly <mshmelly@va-e5.kzn.21-school.ru>
2021-10-04 12:25:31 +0300

    multipipe finished

--- src/commands/ms_pipe.c
+++ src/commands/ms_pipe.c
@@ -23,53 +23,6 @@ size_t	ms_lstsize(t_cmd *arg)
 	}
 	return (i);
 }
-//
-//void	child_pros(char **argv, char **envp, int *fd, t_msh *msh)
-//{
-//	int in, out;
-//
-//	in = open(argv[1], O_RDONLY, 0777);
-//	if (in == -1)
-//		error();
-//	dup2(msh->cmd->out, STDOUT_FILENO);
-//	dup2(in, STDIN_FILENO);
-//	close(msh->cmd->in);
-//	run_command(msh, msh->cmd);
-//}
-//
-//void	parent_pros(char **argv, char **envp, int *fd, t_msh *msh)
-//{
-//	int	out;
-//
-//	out = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-//	if (out == -1)
-//		error();
-//	dup2(msh->cmd->in, STDIN_FILENO);
-//	dup2(out, STDOUT_FILENO);
-//	close(msh->cmd->out);
-//	run_command(msh, msh->cmd);
-//}
-//
-//int ms_pipe(char **argv, char **envp, t_msh *msh)
-//{
-//	int		fd[2];
-//	int		pid;
-//
-//	if (pipe(fd) == -1)
-//		error();
-//	pid = fork();
-//	if (pid == -1)
-//		error();
-//	if (pid == 0)
-//	{
-//		child_pros(argv, envp, fd, msh);
-//	}
-//	wait(NULL);
-//	parent_pros(argv, envp, fd, msh);
-//	close(fd[0]);
-//	close(fd[1]);
-//	return (0);
-//}
 
 int ms_pipex(t_msh *msh, t_cmd *cmd)
 {
@@ -89,8 +42,6 @@ int ms_pipex(t_msh *msh, t_cmd *cmd)
 	while (len--)
 	{
 		first_cmd++;
-		
-		
 		cmd->pid = fork();
 		if (cmd->pid < 0)
 			ms_error(NULL);
@@ -98,16 +49,17 @@ int ms_pipex(t_msh *msh, t_cmd *cmd)
 		{
 			if (first_cmd == 1)
 			{
-				dup2(cmd->pipe_fd[1], 1);//Теперь stdout (1) указывает на дескриптор fd,хотя он все еще равен 1, но теперь вывод перенаправляется.
+				dup2(cmd->pipe_fd[1], 1);
 			}
 			if (!cmd->next)
 			{
 				dup2(msh->old_out, 0);
 			}
-//			else if (first_cmd == 1 && cmd->next)
-//			{
-//				dup2(cmd->pipe_fd[1], 1)
-//			}
+			else if (cmd->next && first_cmd > 1)
+			{
+				dup2(msh->old_out, 0);
+				dup2(cmd->pipe_fd[1], 1);
+			}
 			while (start->next)
 			{
 				close(start->pipe_fd[0]);
@@ -127,7 +79,7 @@ int ms_pipex(t_msh *msh, t_cmd *cmd)
 }
 
 
-int	main(int argc, char **argv, char **envp)
+int	main (int argc, char **argv, char **envp)
 {
 	t_msh	msh;
 	t_cmd	cmd;
@@ -135,16 +87,22 @@ int	main(int argc, char **argv, char **envp)
 	int i = 0;
 	
 //	msh.old_in = 0;
-//	msh.old_out = 1;
+//	msh.old_out = 0;
 	msh.cmd = &cmd;
 	(void)argc;
-	msh.cmd->arg = ft_split("ls -la", ' ');
+	msh.cmd->arg = ft_split("cat file", ' ');
 	msh.cmd->next = malloc(sizeof(*msh.cmd) * 1);
-	msh.cmd->next->arg = ft_split("wc", ' ');
-	msh.cmd->next->next = NULL;
-//	msh.cmd->next->next  = malloc(sizeof(*msh.cmd) * 1);
-//	msh.cmd->next->next->arg = ft_split("cat -e", ' ');
-//	msh.cmd->next->next->next = NULL;
+	msh.cmd->next->arg = ft_split("cat", ' ');
+	msh.cmd->next->next  = malloc(sizeof(*msh.cmd) * 1);
+	msh.cmd->next->next->arg = ft_split("wc", ' ');
+	msh.cmd->next->next->next = NULL;
+//	msh.cmd->next->next->next  = malloc(sizeof(*msh.cmd) * 1);
+//	msh.cmd->next->next->next->arg = ft_split("wc", ' ');
+//	msh.cmd->next->next->next->next  = malloc(sizeof(*msh.cmd) * 1);
+//	msh.cmd->next->next->next->next->arg = ft_split("wc", ' ');
+//	msh.cmd->next->next->next->next->next  = malloc(sizeof(*msh.cmd) * 1);
+//	msh.cmd->next->next->next->next->next->arg = ft_split("cat -e", ' ');
+//	msh.cmd->next->next->next->next->next->next = NULL;
 	msh.envp_l = ms_clone_envp(envp);
 	ms_cp_envp(&msh, envp);
 	ms_pipex(&msh, &cmd);

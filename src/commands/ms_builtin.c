ea3038d625d764d2e12d4093104df8b256bbc872
Melinda Shmelly <mshmelly@va-f2.kzn.21-school.ru>
2021-10-17 14:27:13 +0300

    PASS: 109 / 115 (94%)

--- src/commands/ms_builtin.c
+++ src/commands/ms_builtin.c
@@ -2,22 +2,20 @@
 
 void	ms_builtins(t_msh *msh, t_cmd *cmd)
 {
-	if (!ft_strncmp(cmd->cmd[0], "echo", 4))
+	if (!ft_strcmp(cmd->cmd[0], "echo"))
 		g_status.exit = ms_echo(cmd->cmd);
-	else if (!ft_strncmp(cmd->cmd[0], "cd", 2))
+	else if (!ft_strcmp(cmd->cmd[0], "cd"))
 		g_status.exit = ms_cd(msh, cmd->cmd);
-	else if (!ft_strncmp(cmd->cmd[0], "pwd", 3))
+	else if (!ft_strcmp(cmd->cmd[0], "pwd"))
 		g_status.exit = ms_pwd();
-	else if (!ft_strncmp(cmd->cmd[0], "export", 6))
+	else if (!ft_strcmp(cmd->cmd[0], "export"))
 		g_status.exit = ms_export(msh, cmd->cmd);
-	else if (!ft_strncmp(cmd->cmd[0], "unset", 5))
+	else if (!ft_strcmp(cmd->cmd[0], "unset"))
 		g_status.exit = ms_unset(msh, cmd->cmd);
-	else if (!ft_strncmp(cmd->cmd[0], "env", 3))
+	else if (!ft_strcmp(cmd->cmd[0], "env"))
 		g_status.exit = ms_env(msh->envp_l);
-	else if (!ft_strncmp(cmd->cmd[0], "exit", 4))
+	else if (!ft_strcmp(cmd->cmd[0], "exit"))
 		g_status.exit = ms_exit(msh, cmd->cmd);
-	else
-		ft_putendl_fd("WTF?", 1);
 	return ;
 }
 
@@ -25,19 +23,19 @@ int	is_builtins(char *key)
 {
 	if (key == NULL)
 		return (0);
-	else if (!ft_strncmp(key, "echo", 4))
+	else if (!ft_strcmp(key, "echo"))
 		return (1);
-	else if (!ft_strncmp(key, "cd", 2))
+	else if (!ft_strcmp(key, "cd"))
 		return (1);
-	else if (!ft_strncmp(key, "pwd", 3))
+	else if (!ft_strcmp(key, "pwd"))
 		return (1);
-	else if (!ft_strncmp(key, "export", 6))
+	else if (!ft_strcmp(key, "export"))
 		return (1);
-	else if (!ft_strncmp(key, "unset", 5))
+	else if (!ft_strcmp(key, "unset"))
 		return (1);
-	else if (!ft_strncmp(key, "env", 3))
+	else if (!ft_strcmp(key, "env"))
 		return (1);
-	else if (!ft_strncmp(key, "exit", 4))
+	else if (!ft_strcmp(key, "exit"))
 		return (1);
 	else
 		return (0);
